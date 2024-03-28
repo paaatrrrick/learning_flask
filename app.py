7e190ee7d9989b4fd7d3604df799b3a941f592e2
rkoshal12 <145715441+rkoshal12@users.noreply.github.com>
2024-03-28 11:49:30 -0500

    python file and about page

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
+storage = []
+
+@app.route('/add', methods=['POST'])
+def post():
+    formData = dict(request.form)
+    storage.append(formData)
+    return render_template('index.html', storage=storage)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', storage=storage)
 
-@app.route('/mike')
+@app.route('/about')
 def mike():
-    return render_template('mike.html')
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
